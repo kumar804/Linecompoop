6ede8982a5772b5b6942eb37dc2d55a92b9b1371
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-19 16:33:04 +0530

    calculating length

--- LineCompOop.java
+++ LineCompOop.java
@@ -1,7 +1,24 @@
 package linecompoop;
 
+import java.util.Scanner;
+
 public class LineCompOop {
     public static void main(String[] args) {
         System.out.println("Welcome To LineComparison");
+        //length of line
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of  co-ordinate ");
+
+        System.out.println("Enter the value of (x1,y1)= ");
+        int x1 = sc.nextInt();
+        int y1 =sc.nextInt();
+        System.out.println("Enter the value of (x2,y2)= ");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        double distanceLine1=Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+
+        System.out.println("lenth of line1= "+ distanceLine1);
+
     }
 }
